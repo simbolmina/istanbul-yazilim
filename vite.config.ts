6651e28d89995c568eb7b8fa1f57416e4c249509
simbolmina <simbolmina@gmail.com>
2022-06-07 10:48:05 +0300

    change url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  base: "https://github.com/simbolmina/istanbul-yazilim/blob/produc/vite.config.ts",
+  base: "https://istanbul-yazilim.vercel.app/",
   // server: {
   //   host: true,
   // },
